13e7dd1fd63e8451671eff3514f77b589a5d656b
smit modi <smodi8@asu.edu>
2021-11-03 03:35:50 -0700

    Refactor main.py

--- main.py
+++ main.py
@@ -4,45 +4,46 @@ import os
 from handshape_feature_extractor import HandShapeFeatureExtractor
 import frameextractor
 from scipy import spatial
+
 model = HandShapeFeatureExtractor.get_instance()
 
 
-def generatePenultimateLayer(inputPathName):
+def generate_penultimate_layer(input_path_name):
     videos = []
-    for fileName in os.listdir(inputPathName):
+    for fileName in os.listdir(input_path_name):
         if fileName.endswith(".mp4"):
-            videos.append(os.path.join(inputPathName, fileName))
-    featureVectors = []
-    print("Extracting Frames of " + inputPathName)
+            videos.append(os.path.join(input_path_name, fileName))
+    feature_vectors = []
+    print("Extracting Frames of " + input_path_name)
     for video in videos:
         frame = frameextractor.frame_extractor(video)
         feature = model.extract_feature(frame)
-        featureVectors.append(feature)
-    return featureVectors
+        feature_vectors.append(feature)
+    return feature_vectors
+
 
 # =============================================================================
 # Get the penultimate layer for training data
 # =============================================================================
-training = generatePenultimateLayer("test")
+training = generate_penultimate_layer("traindata")
 
 # =============================================================================
 # Get the penultimate layer for test data (Our Data)
 # =============================================================================
-testing = generatePenultimateLayer("test")
+testing = generate_penultimate_layer("test")
 
 # =============================================================================
 # Recognize the gesture (use cosine similarity for comparing the vectors)
 # =============================================================================
 #
-featureLabel = []
-cosineSimilarity = []
+feature_label = []
+cosine_similarity = []
 for i in range(3):
     for test in testing:
         for train in training:
             dist = spatial.distance.cosine(test, train)
-            cosineSimilarity.append(dist)
-        featureLabel.append(int(cosineSimilarity.index(min(cosineSimilarity))) % 17)
-        cosineSimilarity = []
+            cosine_similarity.append(dist)
+        feature_label.append(int(cosine_similarity.index(min(cosine_similarity))) % 17)
+        cosine_similarity = []
 
-
-np.savetxt("Results.csv", featureLabel, fmt="%d")
+np.savetxt("Results.csv", feature_label, fmt="%d")
